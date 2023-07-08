1d4151de7fa67aee95a854f1d116d898c85b5d0c
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-08 03:18:01 -0300

    NavBar Responsive

--- portfolio/src/components/views/Navbar.jsx
+++ portfolio/src/components/views/Navbar.jsx
@@ -13,10 +13,10 @@ const Navbar=()=>{
             <FontAwesomeIcon icon={faBars} onClick={toggleMenu} className='m-4 mt-3 float-right md:hidden w-7 h-7'/>
             <div className="md:flex justify-between">
                 <span className="block text-3xl mt-2 mr-40 md:flex md:mt-1 md:text-2xl md:ml-5 md:mr-20 lg:text-3xl lg:mr-0 mt-1 lg:ml-10">DenisRoldan.dev</span>
-                <ul className={`${isMenuOpen ? 'block w-full mt-5' : 'hidden'} md:w-fit md:flex md:mt-3 my-2 mr-8 mt-3 text-lg hover:transition-all duration-300}`}>
-                    <li className="w-full border border-solid border-black border-t-0  md:border-none  flex-2 mr-8 cursor-pointer hover:text-blue-500">About me</li>
-                    <li className="w-full border border-solid border-black border-t-0  md:border-none  flex-2 mr-8 cursor-pointer hover:text-blue-500">Skills</li>
-                    <li className="w-full border border-solid border-black border-t-0  md:border-none  flex-2 mr-8 cursor-pointer hover:text-blue-500">Projects</li>
+                <ul className={`${isMenuOpen ? 'block w-full mt-5' : 'hidden'} text-black md:w-fit md:text-white md:flex md:mt-3 my-2 mr-8 mt-3 text-lg hover:transition-all duration-300}`}>
+                    <li onClick={toggleMenu} className="w-full border border-solid h-12 border-black border-t-0  md:border-none  flex-2 mr-8 cursor-pointer hover:text-blue-500">About me</li>
+                    <li onClick={toggleMenu} className="w-full border border-solid h-12 border-black border-t-0  md:border-none  flex-2 mr-8 cursor-pointer hover:text-blue-500">Skills</li>
+                    <li onClick={toggleMenu} className="w-full border border-solid h-12 border-black border-t-0  md:border-none  flex-2 mr-8 cursor-pointer hover:text-blue-500">Projects</li>
                 </ul>
                 <button className="hidden md:flex h-12 w-36  bg-blue-500 justify-around border-0 hover:bg-blue-700">Contact me</button>
             </div>
