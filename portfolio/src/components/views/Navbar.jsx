17d353660ffefc5200f8177456a438efb5000107
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-07 23:54:01 -0300

    NavBar Style

--- portfolio/src/components/views/Navbar.jsx
+++ portfolio/src/components/views/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar=()=>{
     return (
-        <nav className="h-20  w-full bg-slate-300 fixed top-0 left-0 p-4 px-36 text-white tracking-wide shadow-md shadow-bottom">
+        <nav className="h-20  w-full bg-blue-600 font-semibold  fixed top-0 left-0 p-4 px-36 text-white tracking-wide shadow-md shadow-bottom">
             <div className="flex justify-between">
             <span className="text-3xl mt-1 ml-10">DenisRoldan.dev</span>
             <ul className="flex my-2 mr-8 mt-3 text-lg hover:transition-all duration-300">
@@ -8,7 +8,7 @@ const Navbar=()=>{
                 <li className="flex-2 mr-8 cursor-pointer hover:text-blue-500">Skills</li>
                 <li className="flex-2 mr-8 cursor-pointer hover:text-blue-500">Projects</li>
             </ul>
-            <button className="flex h-12 w-36 bg-blue-500 justify-around hover:bg-blue-700">Contact me</button>
+            <button className="flex h-12 w-36  bg-blue-500 justify-around border-0 hover:bg-blue-700 ">Contact me</button>
             </div>
         </nav>
     )
