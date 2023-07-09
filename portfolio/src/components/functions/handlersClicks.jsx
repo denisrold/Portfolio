d0376bd7e1546895b1dacb2b67c36ab7e4fe1008
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-09 02:42:12 -0300

    Footer Finish

---
+++ portfolio/src/components/functions/handlersClicks.jsx
@@ -0,0 +1,9 @@
+const handleClickGitHub = () => {
+    window.open("https://github.com/denisrold", "_blank");
+  };
+  const handleClickLinked = () => {
+    window.open("https://www.linkedin.com/in/denisrold/", "_blank");
+  };
+
+export { handleClickGitHub, handleClickLinked };
+  
--- portfolio/src/components/views/Footer.jsx
+++ portfolio/src/components/views/Footer.jsx
@@ -1,15 +1,16 @@
 import  iconGH from '../../assets/icons/GITH.png';
 import iconLIN from '../../assets/icons/LINKED.png';
+import { handleClickGitHub, handleClickLinked } from'../functions/handlersClicks'
 
 
 
 const Footer=()=>{
     return (
-        <footer className=" absolute flex justify-center space-x-16 md:space-x-48 lg:space-x-72 w-full h-32 left-0 bottom-0 bg-slate-600 ">
-            <p className="inline-block text-2x1 text-white font-semibold mt-12">Copyright © 2023. All rights are reserved</p>
-           <div className='flex space-x-3 mt-auto mb-auto'> 
-            <img src={iconGH} alt="Github logo" className='w-8 h-8 hover:scale-110 cursor-pointer'/>
-            <img src={iconLIN} alt='LinkedIn logo' className='w-8 h-8 hover:scale-110 cursor-pointer' />
+        <footer className=" absolute flex flex-col-reverse items-center justify-center space-x-3 md:items-start md:flex-row md:space-x-48 lg:space-x-72 w-full h-32 left-0 bottom-0 bg-slate-600 ">
+            <p className="text-2x1 text-white font-semibold mt-0 mb-6 md:mt-12">Copyright © 2023. All rights are reserved</p>
+           <div className='flex space-x-16 md:space-x-6 mt-auto mb-auto'> 
+            <img src={iconGH} onClick={handleClickGitHub} alt="Github logo" className='w-8 h-8 hover:scale-110 cursor-pointer'/>
+            <img src={iconLIN} onClick={handleClickLinked}alt='LinkedIn logo' className='w-8 h-8 hover:scale-110 cursor-pointer' />
            </div>
         </footer>
     )
