c625269227937e5077d63a881aacb89b5b26ca64
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-10 13:42:07 -0300

    Responsive home

--- portfolio/src/views/Home.jsx
+++ portfolio/src/views/Home.jsx
@@ -5,11 +5,13 @@ const Home=()=>{
         <main className='flex flex-col items-center md:flex-row md:justify-center space-x-36'>
             <div className='flex flex-col items-start text-start'>
                 <p className="font-bold text-4xl mt-auto">Hi there! i'm <span className='text-blue-700'>Denis</span></p>
-                <p className="text-2xl mt-2 text-start font-semibold ">Full Stack Web Developer <span className="hidden md:inline-block md:text-xl lg:text-2xl">based in Rio Negro, Argentina.</span></p>
-                <span className="block left-auto row-auto text-xl md:hidden">based in Rio Negro, Argentina.</span>
+                <p className="ml-auto mr-auto text-2xl mt-2 text-start font-semibold ">Full Stack Web Developer <span className="hidden md:inline-block md:text-xl lg:text-2xl">based in Rio Negro, Argentina.</span></p>
+                <span className="ml-auto mr-auto block left-auto row-auto text-xl md:hidden">based in Rio Negro, Argentina.</span>
+                <div className="ml-auto mr-auto md:ml-0 md:mr-0">               
                 <IconsHAndlersHome />
+                </div>
             </div>
-            <div className='w-full md:w-96 h-72 mt-8 md:mt-0 border border-black'>soy una imagen</div>
+            <div className=' w-full md:w-96 h-72 mt-8 md:mt-0 border border-black'>soy una imagen</div>
         </main>
     )
     }
