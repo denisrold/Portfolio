e596362ff2fa39340d61c6f3b33529b6b78a5af7
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-10 12:39:56 -0300

    Home config

--- portfolio/src/views/Home.jsx
+++ portfolio/src/views/Home.jsx
@@ -2,10 +2,11 @@ import { IconsHAndlersHome } from "../components/iconsHandlers";
 
 const Home=()=>{
     return (
-        <main className='flex justify-center space-x-36'>
-            <div className='flex flex-col items-start'>
+        <main className='flex flex-col md:flex-row md:justify-center space-x-36'>
+            <div className='flex flex-col items-start text-start'>
                 <p className="font-bold text-4xl mt-auto">Hi there! i'm <span className='text-blue-700'>Denis</span></p>
-                <p className="text-base font-semibold ">Full Stack Web Developer based in Rio Negro, Argentina.</p>
+                <p className="text-2xl text-start font-semibold ">Full Stack Web Developer <span className="hidden md:inline-block md:text-xl lg:text-2xl">based in Rio Negro, Argentina.</span></p>
+                <span className="block left-auto row-auto text-xl md:hidden">based in Rio Negro, Argentina.</span>
                 <IconsHAndlersHome/>
             </div>
             <div className='w-96 h-72 border border-black'>Soy una imagen</div>
