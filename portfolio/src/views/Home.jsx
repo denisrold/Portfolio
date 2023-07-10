70fd6fc785615bc8b67356214bbf6ac6d9267613
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-10 13:45:23 -0300

    Responsive home

--- portfolio/src/views/Home.jsx
+++ portfolio/src/views/Home.jsx
@@ -2,7 +2,7 @@ import { IconsHAndlersHome } from "../components/iconsHandlers";
 
 const Home=()=>{
     return (
-        <main className='flex flex-col items-center md:flex-row md:justify-center space-x-36'>
+        <main className='flex flex-col items-center mb-14 md:flex-row md:justify-center space-x-36'>
             <div className='flex flex-col items-start text-start'>
                 <p className="font-bold text-4xl mt-auto">Hi there! i'm <span className='text-blue-700'>Denis</span></p>
                 <p className="ml-auto mr-auto text-2xl mt-2 text-start font-semibold ">Full Stack Web Developer <span className="hidden md:inline-block md:text-xl lg:text-2xl">based in Rio Negro, Argentina.</span></p>
