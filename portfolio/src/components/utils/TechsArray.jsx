81485f2feca1c871bc1778ebf5147f29e23e5eb9
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-12 20:07:33 -0300

    Tech Stack Responsive

--- portfolio/src/components/utils/TechsArray.jsx
+++ portfolio/src/components/utils/TechsArray.jsx
@@ -1,14 +1,32 @@
 const TechArray = [
 {
+    name:"html5",
+    url:"https://www.w3.org/html/",
+    img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+},
+{
     name:"CSS3", 
     url:"https://www.w3schools.com/css/",
     img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
 },
 {
+    name:"javascript",
+    url:"https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" },
+{
     name:"JAVA", 
     url:"https://www.java.com",
     img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg"
 },
+{
+    name:"react",
+    url:"https://reactjs.org/",
+    img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"},
+    
+{  
+    name:"redux",
+   url:"https://redux.js.org",
+   img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg"},
 {
     name:"spring",
     url:"https://spring.io/",
@@ -19,18 +37,12 @@ const TechArray = [
     url:"https://expressjs.com",
     img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
 },
+
 {
-    name:"git",
-    url:"https://git-scm.com/",
-    img:"https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"},
-{
-    name:"html5",
-    url:"https://www.w3.org/html/",
-    img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"},
-{
-    name:"javascript",
-    url:"https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-    img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" },
+    name:"postgresql",
+    url:"https://www.postgresql.org",
+    img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"},
+    
 {
     name:"mysql",
     url:"https://www.mysql.com/",
@@ -40,17 +52,10 @@ const TechArray = [
     url:"https://nodejs.org",
     img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"},
 {
-    name:"postgresql",
-    url:"https://www.postgresql.org",
-    img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"},
-{
-    name:"react",
-    url:"https://reactjs.org/",
-    img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"},
-{  
-     name:"redux",
-    url:"https://redux.js.org",
-    img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg"}
+    name:"git",
+    url:"https://git-scm.com/",
+    img:"https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"}
+
 ];
 
 export default TechArray;
--- portfolio/src/views/TechStack.jsx
+++ portfolio/src/views/TechStack.jsx
@@ -3,13 +3,13 @@ import TechsArray from "../components/utils/TechsArray"
 const TechStack =()=>{
     return (
         <div className="flex flex-col mx-auto md:flex-row justify-center space-x-4 space-y-2 md:space-y-0 md:space-x-10">
-            <p className="mt-auto mb-auto text-lg font-bold">Tech Stack |</p>
+            <p className="ml-10 mt-auto mb-auto text-lg font-bold">Tech Stack  |</p>
             <div className=" border border-black h-20 flex flex-row space-x-2">
                 {TechsArray.map(s=>{
                   return (
                     <div className="my-auto border rounded-full w-14 h-14 shadow-xl ">
                         <a href={s.url} target="_blank" rel="noreferrer"> 
-                        <img src={s.img} alt={s.name} className=" mx-auto mt-1 max-w-2xl w-13 h-10  hover:brightness-110"/>
+                        <img src={s.img} alt={s.name} className=" mx-auto mt-2 max-w-2xl w-13 h-10 saturate-150 hover:brightness-110 hover:shadow-blue-950"/>
                         </a>
                     </div>)
                 })
