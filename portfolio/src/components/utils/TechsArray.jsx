c6cdf78dc8b7f3114e2bbe75f1a60b351d82d68b
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-12 19:05:25 -0300

    Techs Info

--- portfolio/src/components/utils/TechsArray.jsx
+++ portfolio/src/components/utils/TechsArray.jsx
@@ -1,11 +1,54 @@
 let array = [
-{name:"CSS3", 
-url:"https://www.w3schools.com/css/",
-
+{
+    name:"CSS3", 
+    url:"https://www.w3schools.com/css/",
+    img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
 },
-{},
-{},
-{},
-{},
-{}
+{
+    name:"JAVA", 
+    url:"https://www.java.com",
+    img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg"
+},
+{
+    name:"spring",
+    url:"https://spring.io/",
+    img:"https://www.vectorlogo.zone/logos/springio/springio-icon.svg"
+},
+{
+    name:"express",
+    url:"https://expressjs.com",
+    img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
+},
+{
+    name:"git",
+    url:"https://git-scm.com/",
+    img:"https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"},
+{
+    name:"https://www.w3.org/html/" ,
+    url:"https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+    img:"html5"},
+{
+    name:"javascript",
+    url:"https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" },
+{
+    name:"mysql",
+    url:"https://www.mysql.com/",
+    img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"},
+{
+    name:"nodejs",
+    url:"https://nodejs.org",
+    img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"},
+{
+    name:"postgresql",
+    url:"https://www.postgresql.org",
+    img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"},
+{
+    name:"react",
+    url:"https://reactjs.org/",
+    img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"},
+{  
+     name:"redux",
+    url:"https://redux.js.org",
+    img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg"}
 ];
