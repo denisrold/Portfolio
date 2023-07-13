7b4f54827712dca652122bf425069e0435848265
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-12 23:36:45 -0300

    Responsive

--- portfolio/src/views/TechStack.jsx
+++ portfolio/src/views/TechStack.jsx
@@ -2,14 +2,14 @@ import TechsArray from "../components/utils/TechsArray"
 
 const TechStack =()=>{
     return (
-        <div className="flex flex-col mx-auto md:flex-row justify-center space-x-4 space-y-2 md:space-y-0 md:space-x-10">
-            <p className="ml-10 mt-auto mb-auto text-lg font-bold">Tech Stack  |</p>
-            <div className=" border border-black h-20 flex flex-row space-x-2">
+        <div className="flex flex-col max-w-xs  mx-auto md:flex-row justify-center space-x-4 space-y-2 md:space-y-0 md:space-x-10">
+            <p className="lg:ml-10 mt-auto mb-auto text-lg font-bold">Tech Stack |</p>
+            <div className=" border border-black h-40 md:h-20 flex flex-wrap md:flex-nowrap space-x-2">
                 {TechsArray.map(s=>{
                   return (
-                    <div className="my-auto border rounded-full w-14 h-14 shadow-xl ">
+                    <div className="md:my-auto border rounded-full md:h-14 shadow-xl h-1/2 md:w-14 flex-grow md:flex-row">
                         <a href={s.url} target="_blank" rel="noreferrer"> 
-                        <img src={s.img} alt={s.name} className=" mx-auto mt-2 max-w-2xl w-13 h-10 saturate-150 hover:brightness-110 hover:shadow-blue-950"/>
+                        <img src={s.img} alt={s.name} className="mx-auto mt-2 max-w-2xl w-13 h-10 saturate-150 hover:brightness-110"/>
                         </a>
                     </div>)
                 })
