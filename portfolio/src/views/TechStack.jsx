6ce118f19ab2c8869f7ccc2dedaecef0c00f5733
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-13 21:57:10 -0300

    Next Step

--- portfolio/src/views/TechStack.jsx
+++ portfolio/src/views/TechStack.jsx
@@ -6,18 +6,19 @@ const TechStack =()=>{
             <p className="ml-5 text-xl mb-8 md:-ml-36 lg:ml-10 md:mt-auto md:mb-auto md:text-xl lg:text-lg font-bold">Tech Stack:</p>
             <div className="w-94 border rounded-lg bg-slate-500 h-1 opacity-30  md:hidden"/>
             <div className="p-1 w-fit h-56  md:w-fit  md:h-20 flex flex-wrap md:flex-nowrap space-x-3">
-                {TechsArray.map(s=>{
+                    {TechsArray.map(s=>{
                   return (
                     <div key={s.key} className=" bg-slate-50 w-14  md:my-auto border rounded-full md:h-14 shadow-xl h-16 md:w-14 flex-grow md:flex-row first:ml-3 md:first:ml-0">
                         <a href={s.url} target="_blank" rel="noreferrer"> 
                         <img src={s.img} alt={s.name} className="mx-auto mt-3  md:mt-2 max-w-2xl w-13 h-10 saturate-150 hover:brightness-110"/>
                         </a>
-                    </div>)
+                    </div>
+                  )
                 })
                 }
-           
+            
             </div>
-            <div className="w-94 border rounded-lg bg-slate-500 h-1 opacity-30  md:hidden"/>
+            <div className="w-94 border rounded-lg bg-slate-500 h-1 opacity-30 md:hidden"/>
         </div>
     )
 }
