7deac12b12f5c22cc31490e437fcec97d1e9ada9
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-13 22:32:37 -0300

    About Responsive

--- portfolio/src/views/About.jsx
+++ portfolio/src/views/About.jsx
@@ -1,7 +1,9 @@
 const About = ()=>{
     return (
-        <div className="flex w-full h-60 ">
-            <h1>About Me</h1>
+        <div className="flex flex-col w-full h-fit mt-24 mb-24 items-center space-y-10">
+            <h1 className="">About Me</h1>
+            <div className="w-96 h-96 border border-black">Soy una Imagen</div>
+            <p className="w-96 text-start text-xl ml-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam dolore eaque praesentium odio ex nisi, minus facilis animi culpa autem aliquam maiores asperiores impedit neque voluptates aut veritatis a libero!</p>
         </div>
     )
 }
