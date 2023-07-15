4c56175bd318e261595dc93c2e5774d11693df8b
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-15 00:56:12 -0300

    AboutText Util

---
+++ portfolio/src/components/utils/AboutTexts.jsx
@@ -0,0 +1,5 @@
+export const AboutText = {
+
+    paragraphOne:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam dolore eaque praesentium odio ex nisi, minus facilis animi culpa autem aliquam maiores asperiores impedit neque voluptates aut veritatis a libero!",
+    paragraphTwo:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam dolore eaque praesentium odio ex nisi, minus facilis animi culpa autem aliquam maiores asperiores impedit neque voluptates aut veritatis a libero!"
+}
