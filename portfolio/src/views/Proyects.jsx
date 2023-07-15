df42ed9c69e6f787ef90afc98a33e126c489c0cf
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-15 01:54:37 -0300

    Proyect Responsive

--- portfolio/src/views/Proyects.jsx
+++ portfolio/src/views/Proyects.jsx
@@ -1,18 +1,20 @@
 const Proyects = () =>{
     return (
 
-    <div>
+    <div >
         <div className="text-center mb-10">
         <p className="text-4xl font-semibold">Projects</p>
+
         </div>
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 mt-24 mb-24 border border-black rounded-xl">
-                <div className="mx-auto md:float-right col-span-2 md:col-span-1 mb-10">
-                <div className="w-96 h-96 md:w-96 md:h-96 border border-black">Soy una Imagen</div>
-                </div>
-                <div className="col-span-2 md:col-span-1 md:ml-5 w-full">
-                <p className="w-96 text-start text-xl ml-5 mb-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam dolore eaque praesentium odio ex nisi, minus facilis animi culpa autem aliquam maiores asperiores impedit neque voluptates aut veritatis a libero!</p>
-                </div>
+            <div className="bg-slate-50 max-w-full md:h-80 w-full pt-7 md:pl-2 grid grid-cols-1 gap-4 md:grid-cols-2 mt-24 mb-24 border border-black rounded-xl shadow-2xl">
+                    <div className="mx-auto md:float-right md:pl-3 col-span-2 md:col-span-1 mb-10">
+                        <div className="w-96 h-80 md:w-96 md:h-64 border border-black rounded-xl">Soy una Imagen</div>
+                    </div>
+                    <div className=" ml-2 col-span-2 md:col-span-1 md:ml-5 w-full">
+                        <p className="w-auto md:w-64 text-left text-lg ml-5 md:ml-10 lg:ml-0 mb-6 mr-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam dolore eaque praesentium odio ex nisi, minus facilis animi culpa autem aliquam maiores asperiores impedit neque voluptates aut veritatis a libero!</p>
+                    </div>
             </div>
+
     </div>
     )
 }
