adae506e29e4b14507abd401ce4f1150706d6b82
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-15 03:54:56 -0300

    Responsive proyect

--- portfolio/src/views/Proyects.jsx
+++ portfolio/src/views/Proyects.jsx
@@ -1,13 +1,13 @@
 const Proyects = () =>{
     return (
 
-    <div >
+    <div>
         <div className="text-center mb-10">
         <p className="text-4xl font-semibold">Projects</p>
         </div>
-            <div className="bg-slate-100 max-w-full mt-10 md:h-80 w-full pt-7 md:pl-2 grid grid-cols-1 gap-4 md:grid-cols-2  mb-24 border border-black rounded-xl shadow-2xl">
-                    <div className="mx-auto md:float-right md:pl-3 col-span-2 md:col-span-1 mb-10 md:mr-2">
-                        <div className="w-96 h-80 md:w-96 md:h-64 border md:row-span-2 border-black rounded-xl">Soy una Imagen</div>
+            <div className="bg-slate-100 max-w-screen-lg w-full mt-10 md:h-80 pt-7 grid grid-cols-1 gap-4 md:grid-cols-2  mb-24 border border-black rounded-xl shadow-2xl lg:pr-10">
+                    <div className="mx-auto md:pl-8 lg:pd-5 col-span-2 md:col-span-1 mb-10 md:mr-2">
+                        <div className="w-96 h-80 md:w-96 md:h-64 border  border-black rounded-xl">Soy una Imagen</div>
                     </div>
                     <div className=" ml-2 col-span-1 md:col-span-1 md:ml-5 w-full md:pr-8">
                         <p className="text-center ml-auto mr-auto  w-auto text-xl font-semibold mb-5">Title</p>
