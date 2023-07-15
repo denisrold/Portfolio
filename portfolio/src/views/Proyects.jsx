6ac16189fc570ff098a9e587d15c46db53b39d0d
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-15 04:12:08 -0300

    Responsive correction Proyect

--- portfolio/src/views/Proyects.jsx
+++ portfolio/src/views/Proyects.jsx
@@ -1,8 +1,8 @@
 const Proyects = () =>{
     return (
 
-    <div>
-        <div className="text-center mb-10">
+    <div className="lg:ml-14">
+        <div className="text-center md:-ml-6 lg:-ml-14 mb-10">
         <p className="text-4xl font-semibold">Projects</p>
         </div>
             <div className="bg-slate-100 max-w-screen-lg w-full mt-10 md:h-80 pt-7 grid grid-cols-1 gap-4 md:grid-cols-2  mb-24 border border-black rounded-xl shadow-2xl lg:pr-10">
@@ -12,7 +12,7 @@ const Proyects = () =>{
                     <div className=" ml-2 col-span-1 md:col-span-1 md:ml-5 w-full md:pr-8">
                         <p className="text-center ml-auto mr-auto  w-auto text-xl font-semibold mb-5">Title</p>
                         <p className="text-center w-auto text-base ml-5  mb-6 mr-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam dolore eaque praesentium odio ex nisi, minus facilis animi culpa autem aliquam maiores asperiores impedit neque voluptates aut veritatis a libero!</p>
-                        <div className="ml-auto mr-auto w-80 space-x-8 mb-6 md:mb-0">
+                        <div className="ml-auto mr-auto lg:mt-16 w-80 space-x-8 mb-6 md:mb-0">
                             <button>Button1</button>
                             <button>Button2</button>
                         </div>
