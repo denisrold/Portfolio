b09959ce5889565d7b69e144aef68f295db8eb2e
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-17 18:58:30 -0300

    Contact Effect

--- portfolio/src/views/Contact.jsx
+++ portfolio/src/views/Contact.jsx
@@ -1,10 +1,10 @@
 const ContactMe = ()=>{
     return(
         <>
-        <p>Contact Me</p>
-        <div className="flex flex-row justify-around ml-10">
-        <div>Mail</div>
-        <div>WhatsApp</div>
+        <h1 className="text-4xl font-semibold mb-10">Contact Me</h1>
+        <div className="flex flex-row justify-around ml-8 mb-10">
+        <div className=" border border-black w-96 p-5 h-32 rounded-lg hover:scale-110 transition-transform duration-500">Mail</div>
+        <div className=" border border-black w-96 p-5 rounded-lg hover:scale-110 transition-transform duration-500">WhatsApp</div>
         </div>
         </>
     )
