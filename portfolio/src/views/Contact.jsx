22283b8cf3ffcddac94e8c18dc6996c2e6323169
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-17 20:08:57 -0300

    Styles.modules.css animation en contact

--- portfolio/src/views/Contact.jsx
+++ portfolio/src/views/Contact.jsx
@@ -3,7 +3,8 @@ const ContactMe = ()=>{
         <>
         <h1 className="text-4xl font-semibold mb-10">Contact Me</h1>
         <div className="flex flex-row justify-around ml-8 mb-10">
-        <div className=" border border-black w-96 p-5 h-32 rounded-lg hover:scale-110 transition-transform duration-500">Mail</div>
+        <div className="relative border border-black w-96 p-5 h-32 rounded-lg hover:scale-110 transition-transform duration-500">Mail
+        </div>
         <div className=" border border-black w-96 p-5 rounded-lg hover:scale-110 transition-transform duration-500">WhatsApp</div>
         </div>
         </>
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,10 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html","./src/**/*.{js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {
+      keyframes: {
+        fade: {
+          "0%": { opacity: "1" },
+          "50%": { opacity: "0.5" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        fade: "fade 1s infinite",
+      },
+      keyframes: {
+        animateborde: {
+          "0%": { borderWidth: "0" },
+          "25%": { borderWidth: "0" },
+          "50%": { borderWidth: "2px" },
+          "75%": { borderWidth: "2px" },
+          "100%": { borderWidth: "0" },
+        },
+      },
+      animation: {
+        borde: "animateborde 4s linear infinite", // Duración y repetición de la animación
+      },
     },
   },
   plugins: [],
-}
-
+};
