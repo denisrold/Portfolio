aeadcd6d6e1df993b1536713226e68b2f195cf67
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-18 16:18:14 -0300

    Responsive

--- portfolio/src/assets/styles/TailwindCustom.jsx
+++ portfolio/src/assets/styles/TailwindCustom.jsx
@@ -1,3 +1,5 @@
 export const customList="w-full pt-2 hover:text-blue-700 h-12 border-black border-t-0 active:bg-blue-600 md:active:bg-transparent md:active:scale-95 active:bg-opacity-50 active:text-blue-700 text-blue-500 md:text-white md:border-none md:pt-0 flex-2 mr-8 cursor-pointer md:hover:text-blue-800"
+
 export const customButton="bg-blue-500 justify-around border-0 hover:bg-blue-700 active:scale-95 focus:outline-none focus:bg-none"
-export const customCard="w-96 p-5 h-32 rounded-lg hover:scale-105 transition-transform duration-500 shadow-xl bg-slate-50"
+
+export const customCard="w-96 p-5 h-32 rounded-lg hover:scale-105 transition-transform duration-500 shadow-lg hover:shadow-xl bg-slate-50"
--- portfolio/src/views/Contact.jsx
+++ portfolio/src/views/Contact.jsx
@@ -3,12 +3,13 @@ import { customCard } from "../assets/styles/TailwindCustom";
 const ContactMe = ()=>{
     return(
         <>
-        <h1 className="text-4xl font-semibold mb-10">Contact Me</h1>
-        <div className="flex flex-row justify-around ml-8 mb-10">
-        <div className={`${customCard} "+" ${Styles.card}`}>
-            Email
-        </div>
-        <div className={`${customCard} "+" ${Styles.card}`}>WhatsApp</div>
+        <h1 className="text-4xl font-semibold mb-12 md:mb-10">Contact Me</h1>
+        <div className="flex flex-col w-auto md:flex-row gap-8 md:gap-5 items-center md:justify-around lg:ml-8 mb-20">
+            <div className={`${customCard} "+" ${Styles.card}`}>
+                Email
+            </div>
+            <div className={`${customCard} "+" ${Styles.card}`}>WhatsApp
+            </div>
         </div>
         </>
     )
