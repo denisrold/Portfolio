6b484c7149189695db9bd30fe82c87f251cd7234
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-18 15:39:37 -0300

    ok

--- portfolio/src/views/Contact.jsx
+++ portfolio/src/views/Contact.jsx
@@ -1,12 +1,14 @@
 import Styles from"../components/contactMe/Contact.module.css"
+import { customCard } from "../assets/styles/TailwindCustom";
 const ContactMe = ()=>{
     return(
         <>
         <h1 className="text-4xl font-semibold mb-10">Contact Me</h1>
         <div className="flex flex-row justify-around ml-8 mb-10">
-        <div className={`w-96 p-5 h-32 rounded-lg hover:scale-105 transition-transform duration-500 shadow-xl bg-slate-50 ${Styles.card}`}>
+        <div className={`${customCard} "+" ${Styles.card}`}>
+            Email
         </div>
-        <div className={`w-96 p-5 h-32 rounded-lg hover:scale-105 transition-transform duration-500 shadow-xl bg-slate-50 ${Styles.card}`}>WhatsApp</div>
+        <div className={`${customCard} "+" ${Styles.card}`}>WhatsApp</div>
         </div>
         </>
     )
