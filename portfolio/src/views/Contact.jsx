68029df401867387bd0567ccd963a6f9cd9612c4
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-18 19:21:23 -0300

    Datos de Contacto

--- portfolio/src/views/Contact.jsx
+++ portfolio/src/views/Contact.jsx
@@ -4,13 +4,13 @@ const ContactMe = ()=>{
     return(
         <>
         <h1 className="text-4xl font-semibold mb-12 md:mb-10 lg:mb-16 lg:ml-6">Contact Me</h1>
-        <div className="flex flex-col w-auto md:flex-row gap-8 md:gap-5 items-center md:justify-around lg:ml-8 mb-20">
+        <div className="flex flex-col w-auto md:flex-row gap-8 md:gap-5 items-center md:justify-around lg:ml-8 mb-32">
             <div className={`${customCard} "+" ${Styles.card} "`}>
                 <div className="flex flex-row space-x-6 pt-2 pl-1 gap-1">
                     <img className="w-16 h-16 border border-black rounded-full shadow-lg"/>
                     <div className="flex flex-col items-start pt-3"> 
-                    <p className="font-semibold">Email</p>
-                    <span className="text-lg">denisroldan.dev@gmail.com</span>
+                    <p className="font-semibold text-lg ">Email</p>
+                    <a href="mailto:denisroldan.dev@gmail.com" target="_blank" className="text-lg cursor-pointer text-gray-700 hover:text-blue-700">denisroldan.dev@gmail.com</a>
                     </div>
                 </div>
             </div>
@@ -18,8 +18,8 @@ const ContactMe = ()=>{
             <div className="flex flex-row space-x-6 pt-2 pl-1 gap-1">
                     <img className="w-16 h-16 border border-black rounded-full shadow-lg"/>
                     <div className="flex flex-col items-start pt-3"> 
-                    <p className="font-semibold">WhatsApp</p>
-                    <span>+54 3417122685</span>
+                    <p className="font-semibold text-lg">WhatsApp</p>
+                    <a href="https://api.whatsapp.com/send?phone=3417122685" target="_blank" className="cursor-pointer text-gray-700 hover:text-blue-700">+543417122685</a>
                     </div>
                 </div>
             </div>
