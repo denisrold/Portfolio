944732f34ce285a010080064f69ba1f0614d2919
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-18 17:54:13 -0300

    Responsive Correction

--- portfolio/src/views/Home.jsx
+++ portfolio/src/views/Home.jsx
@@ -2,7 +2,7 @@ import { IconsHAndlersHome } from "../components/iconsHandlers";
 
 const Home=()=>{
     return (
-        <main className='flex flex-col items-center mb-14 md:flex-row md:justify-center lg:space-x-28'>
+        <main className='flex flex-col items-center mb-14 md:flex-row md:justify-center lg:space-x-28 gap-8'>
             <div className='flex flex-col items-start text-start'>
                 <p className="font-bold text-4xl mt-auto">Hi there! i'm <span className='text-blue-700'>Denis</span></p>
                 <p className="ml-auto mr-auto text-2xl mt-2 text-start font-semibold lg:text-sm">Full Stack Web Developer <span className="hidden md:inline-block md:text-xl lg:text-sm">based in Rio Negro, Argentina.</span></p>
@@ -10,9 +10,9 @@ const Home=()=>{
                 <div className="ml-auto mr-auto mt-6 md-0 md:ml-0 md:mr-0">               
                 <IconsHAndlersHome />
                 </div>
-            <p className='w-72 ml-auto mr-auto h-72 mt-10  md:hidden border border-black'>soy una imagen</p>
+            <div className='w-72 ml-auto mr-auto h-72 mt-10 rounded-full md:hidden border border-black'>img</div>
             </div>
-            <p className='hidden md:flex w-full ml-auto mr-auto md:w-96 h-72 mt-8 md:mt-0 border border-black'>soy una imagen</p>
+            <div className='hidden md:flex w-72 ml-auto mr-auto rounded-full h-72 mt-8 md:mt-0 border border-black'>img</div>
         </main>
     )
     }
--- portfolio/src/views/TechStack.jsx
+++ portfolio/src/views/TechStack.jsx
@@ -2,13 +2,13 @@ import TechsArray from "../components/utils/TechsArray"
 
 const TechStack =()=>{
     return (
-        <div className="mr-auto ml-11 md:-ml-10 flex flex-col  mb-10 max-w-xs lg:mx-auto lg:flex-row justify-center space-x-4 space-y-2 lg:space-y-0 lg:space-x-10">
+        <div className="mr-auto ml-auto md:-ml-10 flex flex-col  mb-10 max-w-xs lg:mx-auto lg:flex-row justify-center space-x-4 space-y-2 lg:space-y-0 lg:space-x-10">
             <p className="ml-5 text-xl mb-8 md:-ml-36 lg:ml-10 md:mt-auto md:mb-auto md:text-xl lg:text-lg font-bold">Tech Stack:</p>
             <div className=" w-94 border rounded-lg bg-slate-500 h-1 opacity-30  md:hidden"/>
             <div className="py-1 pr-2 md:p-1 w-fit h-56  md:w-fit  md:h-20 flex flex-wrap md:flex-nowrap space-x-3">
                     {TechsArray.map(s=>{
                   return (
-                    <div key={s.key} className=" bg-slate-50 w-14  md:my-auto border rounded-full md:h-14 shadow-lg h-16 md:w-14 flex-grow md:flex-row first:ml-3 md:first:ml-0 hover:shadow-md hover:scale-110 transition-all duration-300 ">
+                    <div key={s.key} className=" bg-slate-50 w-14  md:my-auto border rounded-full md:h-14 shadow-lg h-16 md:w-14 flex-grow md:flex-row first:ml-3 md:first:ml-0 hover:shadow-md hover:scale-110  active:scale-100 active:shadow-sm transition-all duration-300 ">
                         <a href={s.url} target="_blank" rel="noreferrer"> 
                         <img src={s.img} alt={s.name} className="mx-auto mt-3  md:mt-2 max-w-2xl w-13 h-10 saturate-150 hover:brightness-110 transition-all duration-150"/>
                         </a>
