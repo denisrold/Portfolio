3f0ed54cf5b19647483f5885af1d91ac0fe14d1f
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-18 13:43:51 -0300

    Effect Contact Card

--- portfolio/src/views/Contact.jsx
+++ portfolio/src/views/Contact.jsx
@@ -1,12 +1,12 @@
-import Styles from"../components/contactme/Contact.module.css"
+import Styles from"../components/contactMe/Contact.module.css"
 const ContactMe = ()=>{
     return(
         <>
         <h1 className="text-4xl font-semibold mb-10">Contact Me</h1>
         <div className="flex flex-row justify-around ml-8 mb-10">
-        <div className={`border border-black w-96 p-5 h-32 rounded-lg hover:scale-110 transition-transform duration-500 ${Styles.card}`}>
+        <div className={`w-96 p-5 h-32 rounded-sm hover:scale-105 transition-transform duration-500 ${Styles.card}`}>
         </div>
-        <div className=" border border-black w-96 p-5 rounded-lg hover:scale-110 transition-transform duration-500">WhatsApp</div>
+        <div className={`w-96 p-5 h-32 rounded-sm hover:scale-105 transition-transform duration-500 ${Styles.card}`}>WhatsApp</div>
         </div>
         </>
     )
