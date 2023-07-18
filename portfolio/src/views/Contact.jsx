ddc012cc061fd660f2b0ee096372b0216d30187c
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-18 16:53:43 -0300

    Resposive button proyects

--- portfolio/src/views/Contact.jsx
+++ portfolio/src/views/Contact.jsx
@@ -5,7 +5,7 @@ const ContactMe = ()=>{
         <>
         <h1 className="text-4xl font-semibold mb-12 md:mb-10">Contact Me</h1>
         <div className="flex flex-col w-auto md:flex-row gap-8 md:gap-5 items-center md:justify-around lg:ml-8 mb-20">
-            <div className={`${customCard} "+" ${Styles.card}`}>
+            <div className={`${customCard} "+" ${Styles.card} `}>
                 Email
             </div>
             <div className={`${customCard} "+" ${Styles.card}`}>WhatsApp
--- portfolio/src/views/Proyects.jsx
+++ portfolio/src/views/Proyects.jsx
@@ -6,14 +6,14 @@ const Proyects = () =>{
         <div className="text-center md:-ml-6 lg:-ml-14 mb-10">
             <p className="text-4xl font-semibold">Projects</p>
         </div>
-        <div className="bg-slate-100 max-w-screen-lg w-full mt-10 md:h-80 pt-7 grid grid-cols-1 gap-4 md:grid-cols-2  mb-24 border border-black rounded-xl shadow-2xl lg:pr-10">
+        <div className="bg-slate-100 max-w-screen-lg w-full   mt-14 md:h-80 pt-7 grid grid-cols-1 gap-4 md:grid-cols-2  mb-24 border border-black rounded-xl shadow-2xl lg:pr-10">
                 <div className="mx-auto md:pl-8 lg:pr-10 col-span-2 md:col-span-1 mb-10 md:mr-2">
                     <div className="w-96 h-80 md:w-96 md:h-64 border border-black rounded-lg">Soy una Imagen</div>
                 </div>
                 <div className=" ml-2 col-span-1 md:col-span-1 md:ml-5 w-full md:pr-8">
                     <p className="text-center ml-auto mr-auto  w-auto text-xl font-semibold mb-5">Title</p>
                     <p className="text-center w-auto text-base ml-5  mb-6 mr-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam dolore eaque praesentium odio ex nisi, minus facilis animi culpa autem aliquam maiores asperiores impedit neque voluptates aut veritatis a libero!</p>
-                    <div className="ml-auto mr-auto lg:mt-16 w-80 space-x-8 mb-6 md:mt-0 md:pb-6">
+                    <div className="ml-auto mr-auto w-80 space-x-8 mb-10 md:mt-auto md:mb-auto md:pt-4 box-border">
                         <button className={`shadow-xl text-white ${customButton}}`}>Button1</button>
                         <button className={`shadow-xl text-white ${customButton}}`}>Button2</button>
                     </div>
