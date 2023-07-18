cfbaa74c008583f32f066245e788d9b02d57ac5f
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-18 20:49:43 -0300

    Icons contact

--- portfolio/src/views/Contact.jsx
+++ portfolio/src/views/Contact.jsx
@@ -1,13 +1,17 @@
 import Styles from"../components/contactMe/Contact.module.css"
 import { customCard } from "../assets/styles/TailwindCustom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import iconMail from"../assets/icons/gmail.png"
+ 
 const ContactMe = ()=>{
     return(
         <>
         <h1 className="text-4xl font-semibold mb-12 md:mb-10 lg:mb-16 lg:ml-6">Contact Me</h1>
         <div className="flex flex-col w-auto md:flex-row gap-8 md:gap-5 items-center md:justify-around lg:ml-8 mb-32">
             <div className={`${customCard} "+" ${Styles.card} "`}>
-                <div className="flex flex-row space-x-6 pt-2 pl-1 gap-1">
-                    <img className="w-16 h-16 border border-black rounded-full shadow-lg"/>
+                <div className="flex flex-row space-x-4 pt-2 pl-3 gap-1">
+                <img src={iconMail} className="w-16 h-16 p-1  rounded-full shadow-lg" />
                     <div className="flex flex-col items-start pt-3"> 
                     <p className="font-semibold text-lg ">Email</p>
                     <a href="mailto:denisroldan.dev@gmail.com" target="_blank" className="text-lg cursor-pointer text-gray-700 hover:text-blue-700">denisroldan.dev@gmail.com</a>
@@ -15,8 +19,8 @@ const ContactMe = ()=>{
                 </div>
             </div>
             <div className={`${customCard} "+" ${Styles.card}`}>
-            <div className="flex flex-row space-x-6 pt-2 pl-1 gap-1">
-                    <img className="w-16 h-16 border border-black rounded-full shadow-lg"/>
+            <div className="flex flex-row space-x-4 pt-2 pl-3 gap-1">
+                    <FontAwesomeIcon icon={faWhatsapp} className="w-16 h-16 p-1  rounded-full shadow-lg" />
                     <div className="flex flex-col items-start pt-3"> 
                     <p className="font-semibold text-lg">WhatsApp</p>
                     <a href="https://api.whatsapp.com/send?phone=3417122685" target="_blank" className="cursor-pointer text-gray-700 hover:text-blue-700">+54 3417122685</a>
