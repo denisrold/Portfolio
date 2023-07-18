5c5eda3f9b739c9bf37b2ac2bac12ca8fd8ff80c
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-18 15:20:36 -0300

    Contact Me Effect card

--- portfolio/src/views/Contact.jsx
+++ portfolio/src/views/Contact.jsx
@@ -4,7 +4,7 @@ const ContactMe = ()=>{
         <>
         <h1 className="text-4xl font-semibold mb-10">Contact Me</h1>
         <div className="flex flex-row justify-around ml-8 mb-10">
-        <div className={`w-96 p-5 h-32 rounded-lg hover:scale-105 transition-transform duration-500 shadow-xl ${Styles.card}`}>
+        <div className={`w-96 p-5 h-32 rounded-lg hover:scale-105 transition-transform duration-500 shadow-xl bg-slate-50 ${Styles.card}`}>
         </div>
         <div className={`w-96 p-5 h-32 rounded-lg hover:scale-105 transition-transform duration-500 shadow-xl bg-slate-50 ${Styles.card}`}>WhatsApp</div>
         </div>
--- portfolio/src/views/Proyects.jsx
+++ portfolio/src/views/Proyects.jsx
@@ -8,7 +8,7 @@ const Proyects = () =>{
         </div>
         <div className="bg-slate-100 max-w-screen-lg w-full mt-10 md:h-80 pt-7 grid grid-cols-1 gap-4 md:grid-cols-2  mb-24 border border-black rounded-xl shadow-2xl lg:pr-10">
                 <div className="mx-auto md:pl-8 lg:pr-10 col-span-2 md:col-span-1 mb-10 md:mr-2">
-                    <div className="w-96 h-80 md:w-96 md:h-64 border border-black rounded-sm">Soy una Imagen</div>
+                    <div className="w-96 h-80 md:w-96 md:h-64 border border-black rounded-lg">Soy una Imagen</div>
                 </div>
                 <div className=" ml-2 col-span-1 md:col-span-1 md:ml-5 w-full md:pr-8">
                     <p className="text-center ml-auto mr-auto  w-auto text-xl font-semibold mb-5">Title</p>
