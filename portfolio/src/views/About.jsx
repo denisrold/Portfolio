e338abaeb7c785aaefab14b969987dd350f31404
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-19 14:55:15 -0300

    Project height

--- portfolio/src/views/About.jsx
+++ portfolio/src/views/About.jsx
@@ -4,9 +4,9 @@ import {forwardRef} from "react"
 const About = (props,aboutRef)=>{
     return (
 
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 mt-24 mb-24">
-        <div className="text-center mb-10 col-span-2">
-            <p ref={aboutRef} className="text-4xl font-semibold">About Me</p>
+    <div ref={aboutRef} className="grid grid-cols-1 gap-4 md:grid-cols-2 mt-24 mb-24">
+        <div  className="text-center mb-10 col-span-2">
+            <p  className="text-4xl font-semibold">About Me</p>
         </div>
         <div className="mx-auto md:float-right col-span-2 md:col-span-1 mb-10">
             <div className="w-96 h-96 md:w-96 md:h-96 border border-black">Soy una Imagen</div>
--- portfolio/src/views/Proyects.jsx
+++ portfolio/src/views/Proyects.jsx
@@ -4,9 +4,10 @@ import {forwardRef} from "react"
 const Proyects = (props,projectsRef) =>{
     return (
 
-    <div className="lg:ml-14">
+    <div ref={projectsRef} className="lg:ml-14">
+        <div className="h-24"></div>
         <div className="text-center md:-ml-6 lg:-ml-14 mb-10">
-            <p ref={projectsRef} className="text-4xl font-semibold">Projects</p>
+            <p  className="text-4xl font-semibold">Projects</p>
         </div>
         <div className="bg-slate-100 max-w-screen-lg w-full   mt-14 md:h-80 pt-7 grid grid-cols-1 gap-4 md:grid-cols-2  mb-24 border border-black rounded-xl shadow-2xl lg:pr-10">
                 <div className="mx-auto md:pl-8 lg:pr-10 col-span-2 md:col-span-1 mb-10 md:mr-2">
