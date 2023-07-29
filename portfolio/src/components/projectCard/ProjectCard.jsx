b7a848731046162265c7bfaad9ff21c545de3839
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-29 15:22:26 -0300

    New viewport

--- portfolio/src/components/projectCard/ProjectCard.jsx
+++ portfolio/src/components/projectCard/ProjectCard.jsx
@@ -5,15 +5,15 @@ import project from "../../assets/images/project.png"
 
  const ProjectCard =() =>{
     return(
-        <div className="bg-slate-100 max-w-screen-lg w-full md:-ml-5 mt-14 md:pt-8 md:pl-3
+        <div className="bg-slate-100 max-w-screen-lg w-full md:ml-auto mt-14 md:pt-8 md:pl-3 lg:-ml-4
          md:h-84 pt-7 grid grid-cols-1 gap-4 md:grid-cols-2 mb-24 border border-black rounded-md shadow-2xl lg:pr-10">
         <div className={`md:ml-8 mx-auto md:h-72 md:w-wbig md:pt-4 md:pl-8 md:mt-1  md:pr-8 rounded-lg shadow-lg col-span-2 md:col-span-1 md:mb-10 md:mr-2 overflow-hidden`}>
         <img src={project} alt="Vista previa del proyecto" className="w-96 md:scale-x-125 md:scale-y-114 h-auto md:w-wbig shadow-md  rounded-lg  object-contain mt-9 md:transition-transform md:hover:-translate-y-3/4 md:duration-long "/>
         </div>
-        <div className=" ml-2 col-span-1 md:pt-4 md:col-span-1 md:ml-5 w-full md:pr-8">
+        <div className=" ml-2 col-span-1 md:pt-4 md:col-span-1 w-full md:w-96 md:mr-auto md:ml-16 md:pr-8">
             <p className="text-center ml-auto mr-auto  w-auto text-xl font-semibold mb-5">Title</p>
-            <p className="text-center w-auto text-base ml-5  mb-6 mr-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam dolore eaque praesentium odio ex nisi, minus facilis animi culpa autem aliquam maiores asperiores impedit neque voluptates aut veritatis a libero!</p>
-            <div className="ml-auto mr-auto w-80 space-x-8 mb-10 md:mt-auto md:mb-auto md:pt-4 box-border">
+            <p className="text-center w-auto text-base ml-5 mb-6 mr-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam dolore eaque praesentium odio ex nisi, minus facilis animi culpa autem aliquam maiores asperiores impedit neque voluptates aut veritatis a libero!</p>
+            <div className="ml-auto mr-auto w-80 lg:ml-15 space-x-8 mb-10 md:mt-auto md:mb-auto md:pt-4 box-border">
             <Link className="text-white hover:text-white" to="/projects">
                 <button className={`shadow-xl ${customTWS.customButton}}`}>
                   Details
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -2,25 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    theme: {
-      screens: {
-        sm: "640px",
-        // => @media (min-width: 640px) { ... }
-
-        md: "768px",
-        // => @media (min-width: 768px) { ... }
-
-        lg: "1024px",
-        // => @media (min-width: 1024px) { ... }
-
-        xl: "1280px",
-        // => @media (min-width: 1280px) { ... }
-
-        "2xl": "1536px",
-        // => @media (min-width: 1536px) { ... }
-      },
-    },
     extend: {
+      screens: {
+        md: "968px",
+        // => @media (min-width: 992px) { ... }
+      },
       transitionDuration: {
         long: "6000ms",
       },
