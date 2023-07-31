996135537daac2153bca331224aa8edf48c8c107
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-07-31 14:32:13 -0300

    Realizado

--- portfolio/src/views/TechStack.jsx
+++ portfolio/src/views/TechStack.jsx
@@ -3,9 +3,9 @@ import TechsArray from "../components/utils/TechsArray"
 const TechStack =()=>{
     return (
         <div className="pr-2 md:pr-0 mr-auto sm:mr-auto ml-auto md:-ml-10 flex flex-col  mb-10 max-w-xs lg:mx-auto lg:flex-row justify-center space-x-4 space-y-2 lg:space-y-0 lg:space-x-10">
-            <p className="text-2xl ml-2 mb-8 md:-ml-36 lg:ml-10 md:mt-auto md:mb-auto md:text-xl lg:text-lg font-bold">Tech Stack</p>
+         
             <div className=" w-94 border rounded-lg bg-slate-500 h-1 opacity-30  md:hidden"/>
-            <div className="py-1 pr-2 md:p-1 w-fit h-56  md:w-fit  md:h-20 flex flex-wrap md:flex-nowrap space-x-3">
+            <div className="py-1 pr-2 md:p-1 w-fit h-56  md:w-fit  md:h-20 flex flex-wrap md:flex-nowrap space-x-3 md:space-x-6 lg:pr-6 ">
                     {TechsArray.map((s,i)=>{
                   return (
                     <div key={i} className=" bg-slate-50 w-14  md:my-auto border rounded-full md:h-14 shadow-lg h-16 md:w-14 flex-grow md:flex-row first:ml-3 md:first:ml-0 hover:shadow-md hover:scale-110  active:scale-100 active:shadow-sm transition-all duration-300 ">
