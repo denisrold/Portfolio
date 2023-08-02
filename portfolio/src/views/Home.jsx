05095eda33bf9392cd760f0465c89464c30f8c03
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-08-01 21:29:00 -0300

    shadows text

--- portfolio/src/views/Home.jsx
+++ portfolio/src/views/Home.jsx
@@ -8,7 +8,7 @@ const Home=(props,homeRef)=>{
         <main className='flex flex-col items-center md:mb-12 mb-14 md:flex-row md:justify-center lg:space-x-28 gap-8 lg:mb-20'>
             <div className='flex flex-col items-start text-start'>
                 <p className="font-bold text-4xl mt-auto">Hi there! i'm <span className='text-blue-700'>Denis</span></p>
-                <p className="ml-auto mr-auto text-2xl mt-2 text-start font-semibold lg:text-sm">Full Stack Web Developer <span className="hidden md:inline-block md:text-xl lg:text-sm">based in Rio Negro, Argentina.</span></p>
+                <p className="ml-auto mr-auto text-2xl mt-2 text-start font-semibold lg:text-base">Full Stack Web Developer <span className="hidden md:inline-block md:text-xl lg:text-base">based in Rio Negro, Argentina.</span></p>
                 <span className="ml-auto mr-auto block left-auto row-auto text-xl font-medium md:hidden">based in Rio Negro, Argentina.</span>
                 <div className="ml-auto mr-auto mt-4 md:mt-6 md-0 md:ml-0 md:mr-0">               
                 <IconsHAndlersHome />
