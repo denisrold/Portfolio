85aacbc050425c1fd73ccc767267660bb1b75d91
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-08-04 20:20:01 -0300

    DarkMode

--- portfolio/src/components/projectCard/ProjectCard.jsx
+++ portfolio/src/components/projectCard/ProjectCard.jsx
@@ -8,7 +8,7 @@ import project from "../../assets/images/project.png"
         setButtonClicked(true);
     }
     return(
-        <div className="bg-slate-100 max-w-screen-lg w-full md:ml-auto mt-14 md:pt-8 md:pl-3 lg:-ml-4
+        <div className="mi-dark-mode bg-slate-100 max-w-screen-lg w-full md:ml-auto mt-14 md:pt-8 md:pl-3 lg:-ml-4
          md:h-84 pt-7 grid grid-cols-1 gap-4 md:grid-cols-2 mb-24 border border-black rounded-md shadow-2xl lg:pr-10">
         <div className={`md:ml-8 mx-auto md:h-72 md:w-wbig md:pt-4 md:pl-8 md:mt-1  md:pr-8 rounded-lg shadow-lg col-span-2 md:col-span-1 md:mb-10 md:mr-2 overflow-hidden`}>
         <img src={project} alt="Vista previa del proyecto" className="w-96 md:scale-x-125 md:scale-y-114 h-auto md:w-wbig shadow-md  rounded-lg  object-contain mt-9 md:transition-transform md:hover:-translate-y-3/4 md:duration-long "/>
--- portfolio/src/views/TechStack.jsx
+++ portfolio/src/views/TechStack.jsx
@@ -8,7 +8,7 @@ const TechStack =()=>{
             <div className="py-1 pr-2 md:p-1 w-fit h-56  md:w-fit  md:h-20 flex flex-wrap md:flex-nowrap space-x-3 md:space-x-6 lg:pr-6 ">
                     {TechsArray.map((s,i)=>{
                   return (
-                    <div key={i} className=" bg-slate-50 w-14  md:my-auto border rounded-full md:h-14 shadow-lg h-16 md:w-14 flex-grow md:flex-row first:ml-3 md:first:ml-0 hover:shadow-md hover:scale-110  active:scale-100 active:shadow-sm transition-all duration-300 ">
+                    <div  key={i} className="mi-dark-mode bg-slate-50 w-14  md:my-auto border rounded-full md:h-14 shadow-lg h-16 md:w-14 flex-grow md:flex-row first:ml-3 md:first:ml-0 hover:shadow-md hover:scale-110  active:scale-100 active:shadow-sm transition-all duration-300 ">
                         <a href={s.url} target="_blank" rel="noreferrer"> 
                         <img src={s.img} alt={s.name} className="mx-auto mt-3  md:mt-2 max-w-2xl w-13 h-10 saturate-150 hover:brightness-110 transition-all duration-150"/>
                         </a>
