8e4803c42a197d7c9ef36cac838a0420f6c4c50e
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-08-05 22:25:09 -0300

    Button toggle

--- portfolio/src/components/darkModeButton/darkModeButton.jsx
+++ portfolio/src/components/darkModeButton/darkModeButton.jsx
@@ -4,7 +4,7 @@ export const DarkModeButton =()=>{
     return(
     <div className='w-full flex justify-end'>
     <div className={Style.container}>
-    <button className={`${Style.toggle} + bg-white  p-0 active:scale-95 focus:outline-none focus:bg-none`}>On</button>
+    <button className={`${Style.toggle} + p-0 focus:outline-none focus:bg-none`}></button>
     </div>
     </div>
  )
